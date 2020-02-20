154f8c506cdad6fecb663b7c31f377299ec2b65b
branonconor <breusebio@westmont.edu>
2020-02-19 17:07:54 -0800

    Nav repari successful, mobile menu much cleaner

--- index.js
+++ index.js
@@ -9,23 +9,16 @@ const expandMenu = () => {
     // take the mobile nav menu and reveal it on a hamburger click
     navMobile.classList.toggle('revealNavMobile');
 }
-const showMobileMenu = () => {
-    // loop through each mobile menu link, remove the "display none" and 
-    for (let i = 0; i< mobileLinks.length; i++) {
-        mobileLinks[i].classList.toggle('display-none');
-    }
-}
+
 
 // Burger animation
 const hamburgerClick = (x) => {
     x.classList.toggle('change');
 }
 
-
 // Expand Mobile Menu
 hamburger.addEventListener('click', function() {
     expandMenu();
-    setTimeout(showMobileMenu, 250);
 });
 
 // Expand cards
